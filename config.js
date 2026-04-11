ac9cff5d84f95ced4c3be08792537fbe9d5be271
ccwwesleychoy-bit <ccwwesleychoy@gmail.com>
2026-04-11 10:05:20 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ window.SHOP_CONFIG = {
     "As drip bags contain fresh roasted coffee, returns and refunds are not accepted except in cases of manufacturing defects or damage during delivery.\n\n" +
     "We appreciate your understanding.",
   payMeUrl: "https://payme.hsbc/996976ef1a4840e397b5d218c81a662a",
-  fpsId: "65459695",
+  fpsId: "128799590",
   fpsNote: "Please put the Order ID in the transfer remark.",
   // Optional: Google Apps Script Web App URL (used on Cloudflare Pages).
   // If set, checkout submit will POST order data to this endpoint to trigger a Gmail email.
